4212d14414ffa33b6edacef0b15bc3c1f7978906
Aaron Pomeranz <aaron.pomeranz@maine.edu>
2026-07-31 00:03:50 -0400

    Post-deploy: update CDN hash refs to @af5f55b62

--- desktop/web-app/public/app/sw.js
+++ desktop/web-app/public/app/sw.js
@@ -4,8 +4,8 @@
 // Other same-origin requests: network-first with cache fallback.
 
 // IMPORTANT: placeholders are replaced by postbuild.js.
-const CACHE_NAME = 'alloflow-student-shell-v1785391114262';
-const PRECACHE_PATHS = ["./index.html","./alloflow_desktop_bridge.js","./static/js/main.4f4d6f66.js","./static/css/main.96ce38a7.css"];
+const CACHE_NAME = 'alloflow-student-shell-v1785470628997';
+const PRECACHE_PATHS = ["./index.html","./alloflow_desktop_bridge.js","./static/js/main.91ec8615.js","./static/css/main.96ce38a7.css"];
 const scopedUrl = (relativePath) => new URL(relativePath, self.registration.scope).toString();
 const SHELL_URL = scopedUrl('./index.html');
 
