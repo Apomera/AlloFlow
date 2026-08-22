21808b83ef6899fab54d8b31183ab45d47ee40f0
Aaron Pomeranz <aaron.pomeranz@maine.edu>
2026-08-22 13:23:45 -0400

    Add OpenBIM to the deployment manifest

--- build.js
+++ build.js
@@ -1302,6 +1302,7 @@ const PLUGIN_FILES = [
     'stem_lab/stem_tool_areamodel.js',
     'stem_lab/stem_tool_artstudio.js',
     'stem_lab/stem_tool_sourcebook.js',
+    'stem_lab/stem_tool_openbim.js',
     'stem_lab/stem_tool_wheelandfire.js',
     'stem_lab/stem_tool_brainatlas.js',
     'stem_lab/stem_tool_cell.js',
